2929929b3055147a3b89cfc26ce891fff3781bcf
Epixl Studio <abdalrahimk083@gmail.com>
2024-03-31 18:52:20 +0300

    add dark-mode using Tailwind

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, HostBinding, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  darkMode = signal<boolean>(
+    JSON.parse(window.localStorage.getItem('darkMode') ?? 'false')
+  );
+
+  @HostBinding('class.dark') get mode() {
+    return this.darkMode();
+  }
+
+  constructor() {
+    effect(() => {
+      window.localStorage.setItem('darkMode', JSON.stringify(this.darkMode()));
+    });
+  }
+}
